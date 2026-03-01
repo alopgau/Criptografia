99107d5e76f2fec4b614cbe971691a0be90cc1d8
alopgau <alopezg@ieslabahia.es>
2026-03-01 23:43:39 +0100

    desencriptación a medias

--- atbash.py
+++ atbash.py
@@ -1,20 +1,23 @@
 def pedir_alfabeto() -> str:
     return input("Introduce el alfabeto en el que basaras la encriptación\n").lower()
-def crear_diccionario(alfabeto:str) -> dict[str,str]:
-    diccionario = {}
+def crear_diccionarios(alfabeto:str) -> dict[str,str]:
+    diccionario_encriptación = {}
+    diccionario_desencriptación = {}
     if " " not in alfabeto:
-        diccionario.setdefault(" ", " ")
+        diccionario_encriptación.setdefault(" ", " ")
+        diccionario_desencriptación.setdefault(" ", " ")
 
     for i in range(len(alfabeto)):
-        diccionario.setdefault(alfabeto[i], alfabeto[len(alfabeto)-1-i])
-    return diccionario
-def pedir_textos(diccionario: dict[str,str]) -> None:
+        diccionario_encriptación.setdefault(alfabeto[i], alfabeto[len(alfabeto)-1-i])
+        diccionario_desencriptación.setdefault(alfabeto[len(alfabeto)-1-i], alfabeto[i])
+    return diccionario_encriptación
+def pedir_textos() -> None:
     entrada = ""
     while entrada != "x":
         entrada = input("Introduce el texto a encriptar (X para salir)\n").lower()
         texto_encriptado = ""
         for c in entrada:
-            texto_encriptado += diccionario[c].upper()
+            texto_encriptado += diccionario_encriptación[c].upper()
         mostrar_encriptación(entrada,texto_encriptado)
 
 def mostrar_encriptación(original:str,encriptado:str) -> None:
@@ -23,7 +26,7 @@ def mostrar_encriptación(original:str,encriptado:str) -> None:
 def main():
  alfabeto = pedir_alfabeto()
  diccionario = crear_diccionario(alfabeto)
- pedir_textos(diccionario)
+ pedir_textos()
 
 
 
